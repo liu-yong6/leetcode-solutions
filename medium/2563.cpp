b867e539146290b39d92ff0c8e4df7fffb43f71d
liu-yong6 <liuyongz6@icloud.com>
2025-11-20 18:59:51 +0800

    2563_排序+二分查找，使用lower_bound和upper_bound函数判断左右边界

--- medium/2563.cpp
+++ medium/2563.cpp
@@ -7,17 +7,17 @@ using namespace std;
 class Solution {
 public:
     long long countFairPairs(vector<int>& nums, int lower, int upper) {
-        long res=0,left;
-        for(int i=1;i<nums.size();i++)
-        {
-            left=0;
-            while(left<i)
-            {
-                int t=nums[i]+nums[left];
-                if(t<=upper&&t>=lower)
-                    res++;
-                left++;
-            }
+        sort(nums.begin(), nums.end());
+        //为什么排序不影响，因为如果i>j,也可以是j>i,只要保证枚举时一个在左，一个在右即可
+        long long res = 0;
+        int n = nums.size();
+        for (int i = 0; i < n; ++i) {
+            //lower_bound 在nums[i+1]到末尾，查找num[i]+nums[left]>lower的第一个
+            int left = lower_bound(nums.begin() + i + 1, nums.end(), lower - nums[i]) - nums.begin();
+            //upper_bound 在nums[i+1]到末尾，查找num[i]+nums[left]<upper的第一个
+            int right = upper_bound(nums.begin() + i + 1, nums.end(), upper - nums[i]) - nums.begin();
+            //且right必然大于left
+            res += right - left;
         }
         return res;
     }
