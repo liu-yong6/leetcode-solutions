84968ff661b8c717a77b4a67e19271f8d81ded53
liu-yong6 <liuyongz6@icloud.com>
2025-11-24 14:14:36 +0800

    1577.重复情况和整个数组都相等情况都需要考虑

--- medium/1577.cpp
+++ medium/1577.cpp
@@ -12,18 +12,38 @@ public:
         sort(nums1.begin(),nums1.end());
         sort(nums2.begin(),nums2.end());
         int len1=nums1.size(),len2=nums2.size();
-        for(int i=len1-1;i>1;i--)
+        // for(int i=len1-1;i>1;i--)
+        //因为现在是两个数组，所以需要遍历到0
+        for(int i=len1-1;i>=0;i--) 
         {
+            //10^5使用pow会溢出
+            long long target = 1LL * nums1[i] * nums1[i];
+
             int left=0,right=len2-1;
             while(left<right)
             {
-                if(nums2[left]*nums2[right]==pow(nums1[i],2))
+                // if(nums2[left]*nums2[right]==pow(nums1[i],2))
+                long long prod = 1LL * nums2[left] * nums2[right];
+                if (prod == target)
                 {
                     ans++;
+                    int l = left, r = right;
+                    
+                    //如果从left到right都相等，那么需要使用公式Cn2 
+                    if (nums2[left] == nums2[right]) {
+                        long long n = right - left + 1;
+                        ans += n * (n - 1) / 2;
+                        break;
+                    }
+                    while (left < right && nums2[left] == nums2[l]) left++;
+                    while (right >= left && nums2[right] == nums2[r]) right--;
+                    ans += (left - l) * (r - right);
                 }
-                else if(nums2[left]*nums2[left]>pow(nums1[i],2))
+                // else if(nums2[left]*nums2[left]>pow(nums1[i],2))
+                else if (prod > target)
                 {
-                    break;
+                    // break;
+                    right--;
                 }
                 else
                     left++;
